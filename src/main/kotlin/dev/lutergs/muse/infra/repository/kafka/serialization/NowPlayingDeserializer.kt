41a674b246bc857ab04782e55df752ae72819eaf
LuterGS <koo04034@gmail.com>
2024-05-16 17:08:52 +0900

    [FIX] NowPlayingDeserializer 가 작동하지 않는 문제 해결

--- src/main/kotlin/dev/lutergs/muse/infra/repository/kafka/serialization/NowPlayingDeserializer.kt
+++ src/main/kotlin/dev/lutergs/muse/infra/repository/kafka/serialization/NowPlayingDeserializer.kt
@@ -1,6 +1,9 @@
 package dev.lutergs.muse.infra.repository.kafka.serialization
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import dev.lutergs.muse.domain.entity.track.MusicVendor
+import dev.lutergs.muse.domain.entity.track.PlaybackStatus
+import dev.lutergs.muse.domain.entity.track.Track
 import dev.lutergs.muse.domain.entity.userInfo.NowPlaying
 import org.apache.kafka.common.serialization.Deserializer
 
@@ -9,6 +12,18 @@ class NowPlayingDeserializer: Deserializer<NowPlaying?> {
 
   override fun deserialize(topicName: String, value: ByteArray): NowPlaying? {
     return if (value.isEmpty()) null
-    else this.objectMapper.readValue(value, NowPlaying::class.java)
+    else this.objectMapper.readTree(value)
+      .let { rootNode ->
+        NowPlaying(
+          track = rootNode.get("track").let { trackNode ->
+            Track(
+              vendor = MusicVendor.valueOf(trackNode.get("vendor").asText()),
+              uid = trackNode.get("uid").asText()
+            )
+          },
+          status = PlaybackStatus.valueOf(rootNode.get("status").asText()),
+          timestamp = rootNode.get("timestamp").asLong()
+        )
+      }
   }
 }
